4feb37e7208b9d98b24125ef97720512e07e5ae0
ARTSID90 <sitdikov365@gmail.com>
2021-02-10 15:36:06 +0300

    1

--- dentist/settings.py
+++ dentist/settings.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 import os
 import django_heroku
 import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
